7b8402327feded1402ffd5a5d4aef78ae411c26f
Tu Dinh <contact@tudinh.xyz>
2025-10-05 04:13:26 +0200

    Build mimalloc from submodule
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -17,8 +17,45 @@ for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Micros
 popd
 call "%VisualStudioInstallDir%\VC\Auxiliary\Build\vcvarsall.bat" x86
 
+git -C mimalloc checkout -f
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git -C mimalloc clean -fxd
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git -C mimalloc apply ..\0001-Secure-build-props.patch
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git -C mimalloc apply ..\0001-Fix-cross-build.patch
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
+rem Build Mimalloc x86
+mkdir Output\x86-secure
+pushd Output\x86-secure
+cmake ..\..\mimalloc -A Win32 -DMI_OVERRIDE=ON -DMI_SECURE=ON
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+cmake --build . --config=Release
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
+rem Build Mimalloc x64
+mkdir Output\x64-secure
+pushd Output\x64-secure
+cmake ..\..\mimalloc -A x64 -DMI_OVERRIDE=ON -DMI_SECURE=ON
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+cmake --build . --config=Release
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
+rem Build Mimalloc arm64
+mkdir Output\arm64-secure
+pushd Output\arm64-secure
+cmake ..\..\mimalloc -A ARM64 -DMI_OVERRIDE=ON -DMI_SECURE=ON
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+cmake --build . --config=Release
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
 rem Build all targets
 MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m BuildAllTargets.proj
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 rem Extract files from NuGet package to folder
 7z x Output\Mile.Mimalloc.nupkg "-o.\Output\Mile.Mimalloc"
